40afaca7d1be2c6e21726f4c0c773e82d09f893a
Motti Wilschanski <mchillywilly@gmail.com>
2021-06-24 17:01:42 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def say_hello (name = "Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_hello("name")
+  puts "greeting #{"Motti"}"
+>>>>>>> 5d788d97dd7edcf57daf81c3aa6a2b498d08d0bc
 end
